6059303eb7d4e36bd1fc851774adf55737637392
dimas <dimaswh19@gmail.com>
2025-11-05 12:17:44 +0700

    Mengubah presensi.js dengan menambahkan middleware validasi

--- 20230140078-node-server/routes/presensi.js
+++ 20230140078-node-server/routes/presensi.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const presensiController = require('../controllers/presensiController');
-const { addUserData } = require('../middleware/permissionMiddleware');
-router.use(addUserData);
-router.post('/check-in', presensiController.CheckIn);
-router.post('/check-out', presensiController.CheckOut);
-router.delete("/:id", presensiController.deletePresensi);
-router.put("/:id", presensiController.updatePresensi);
-module.exports = router;
 
+const { body, validationResult } = require('express-validator');
+
+const updateValidationChain = [
+    body('checkIn').optional().isISO8601().withMessage('Format checkIn harus berupa tanggal/waktu ISO 8601 yang valid.'),
+
+    body('checkOut').optional().isISO8601().withMessage('Format checkOut harus berupa tanggal/waktu ISO 8601 yang valid.'),
+];
+
+
+router.put('/:id', 
+    updateValidationChain, 
+    presensiController.updatePresensi
+);
+
+module.exports = router;
